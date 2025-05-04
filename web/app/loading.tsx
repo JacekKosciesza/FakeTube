eb7a6eedb493adbf8f02847fad5a08d19ce45b1d
Jacek Kościesza <jacek.kosciesza@gmail.com>
2025-05-04 18:50:17 +0200

    feat(appshell): empty appshell component (#12)

---
+++ web/app/loading.tsx
@@ -0,0 +1,5 @@
+import LinearProgress from "@mui/material/LinearProgress";
+
+export default function Loading() {
+  return <LinearProgress color="error" sx={{ height: 3 }} />;
+}
--- web/app/theme.ts
+++ web/app/theme.ts
@@ -2,9 +2,27 @@
 
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+  interface BreakpointOverrides {
+    xxl: true;
+    xxxl: true;
+  }
+}
+
 const theme = createTheme({
   typography: {
     fontFamily: "var(--font-roboto)",
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 601,
+      md: 792,
+      lg: 1313,
+      xl: 1536,
+      xxl: 1920,
+      xxxl: 2985,
+    },
   },
 });
 
