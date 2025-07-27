b971f0fe6d14bbdeecabe3d2ef6af9cfc51b9612
Jacek Kościesza <jacek.kosciesza@gmail.com>
2025-07-27 17:54:52 +0200

    feat(home): mock data (#7)

---
+++ web/app/Home/channel.ts
@@ -0,0 +1,5 @@
+export interface Channel {
+  id: string;
+  avatar: string;
+  name: string;
+}
---
+++ web/app/Home/channels.data.ts
@@ -0,0 +1,9 @@
+import { Channel } from "./channel";
+
+export const CHANNELS: Channel[] = [
+  {
+    id: "AmazonNovaReel",
+    avatar: "/channels/AmazonNovaReel/AmazonNovaReel.png",
+    name: "Amazon Nova Reel",
+  },
+];
